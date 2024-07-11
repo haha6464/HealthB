3e7d7f8809d7c7fa555baf1eefa3091cfa0e3e8d
Dog2077 <1002302241@qq.com>
2024-07-11 10:08:36 +0800

    update:完成后台管理服务分页查询接口

--- web-server/base-modules/base-accompanyCare/src/main/java/com/example/baseaccompanying/service/impl/ServeServiceImpl.java
+++ web-server/base-modules/base-accompanyCare/src/main/java/com/example/baseaccompanying/service/impl/ServeServiceImpl.java
@@ -4,10 +4,14 @@ package com.example.baseaccompanying.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.example.baseaccompanying.dao.*;
+import com.example.baseaccompanying.dao.HospitalMapper;
+import com.example.baseaccompanying.dao.ServeItemMapper;
+import com.example.baseaccompanying.dao.ServeMapper;
+import com.example.baseaccompanying.dao.ServeTypeMapper;
 import com.example.baseaccompanying.service.ServeService;
 import com.example.baseaccompanying.service.ServeToOrderListService;
 import huice.accompaniment.common.constant.ErrorInfo;
+import huice.accompaniment.common.core.PageImpl;
 import huice.accompaniment.common.domain.Hospital;
 import huice.accompaniment.common.domain.Serve;
 import huice.accompaniment.common.domain.ServeItem;
@@ -19,14 +23,11 @@ import huice.accompaniment.common.enums.ServeSaleStatus;
 import huice.accompaniment.common.exception.BadRequestException;
 import huice.accompaniment.common.exception.ForbiddenOperationException;
 import huice.accompaniment.common.utils.ThreadLocalUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import huice.accompaniment.common.core.PageImpl;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * 服务表(Serve)表服务实现类
@@ -74,15 +75,17 @@ public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements
     public PageImpl<?> queryByPage(Serve serve, Integer page, Integer size) {
         Long uid = ThreadLocalUtils.getUid();
         long total = this.serveMapper.count(serve, uid);
-        List<ServePageVo> servePageVos = this.serveMapper.queryAllByLimit(serve, uid, page * size, size).stream().map(o -> {
-            ServePageVo servePageVo = new ServePageVo();
-            ServeItem serveItem = this.serveItemMapper.queryById(o.getId());
-            servePageVo.setServeItem(serveItem);
-            servePageVo.setServeType(this.serveTypeMapper.queryById(serveItem.getServeTypeId()));
-            servePageVo.setHospital(this.hospitalMapper.queryById(o.getHospitalId()));
-            servePageVo.setSold(this.serveToOrderListService.getSoldByServeId(o.getId()));
-            return servePageVo;
-        }).collect(Collectors.toList());
+        List<ServePageVo> servePageVos = this.serveMapper.queryAllByLimit(serve, uid, page * size, size)
+                .stream().map(o -> {
+                    ServePageVo servePageVo = new ServePageVo();
+                    ServeItem serveItem = this.serveItemMapper.queryById(o.getServeItemId());
+                    servePageVo.setServeItem(serveItem);
+                    servePageVo.setServeType(this.serveTypeMapper.queryById(serveItem.getServeTypeId()));
+                    servePageVo.setHospital(this.hospitalMapper.queryById(o.getHospitalId()));
+                    servePageVo.setSold(this.serveToOrderListService.getSoldByServeId(o.getId()));
+                    servePageVo.setCreateTime(o.getCreateTime());
+                    return servePageVo;
+                }).collect(Collectors.toList());
         return new PageImpl<>(servePageVos, total);
     }
 
@@ -106,9 +109,7 @@ public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements
             throw new BadRequestException(ErrorInfo.Msg.REQUEST_PARAM_ILLEGAL + "医院不存在");
         }
 
-        LambdaQueryWrapper<Serve> queryWrapper = Wrappers.<Serve>lambdaQuery()
-                .eq(Serve::getServeItemId, serve.getServeItemId())
-                .eq(Serve::getHospitalId, serve.getHospitalId());
+        LambdaQueryWrapper<Serve> queryWrapper = Wrappers.<Serve>lambdaQuery().eq(Serve::getServeItemId, serve.getServeItemId()).eq(Serve::getHospitalId, serve.getHospitalId());
 
         Long count = baseMapper.selectCount(queryWrapper);
         // 不能重复添加服务项目
@@ -144,9 +145,7 @@ public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements
             throw new BadRequestException(ErrorInfo.Msg.REQUEST_PARAM_ILLEGAL + "医院不存在");
         }
 
-        LambdaQueryWrapper<Serve> queryWrapper = Wrappers.<Serve>lambdaQuery()
-                .eq(Serve::getServeItemId, serve.getServeItemId())
-                .eq(Serve::getHospitalId, serve.getHospitalId());
+        LambdaQueryWrapper<Serve> queryWrapper = Wrappers.<Serve>lambdaQuery().eq(Serve::getServeItemId, serve.getServeItemId()).eq(Serve::getHospitalId, serve.getHospitalId());
 
         Long count = baseMapper.selectCount(queryWrapper);
         // 服务项目不能重复
@@ -169,9 +168,7 @@ public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements
     @Override
     public boolean deleteById(Long id) {
         // 查询是否存在该服务且未被删除
-        LambdaQueryWrapper<Serve> queryWrapper = Wrappers.<Serve>lambdaQuery()
-                .eq(Serve::getId, id)
-                .eq(Serve::getDelFlag, DelFlagEnum.NOT_DELETE.getStatus());
+        LambdaQueryWrapper<Serve> queryWrapper = Wrappers.<Serve>lambdaQuery().eq(Serve::getId, id).eq(Serve::getDelFlag, DelFlagEnum.NOT_DELETE.getStatus());
         Serve serve = baseMapper.selectOne(queryWrapper);
         if (serve == null) {
             throw new BadRequestException(ErrorInfo.Msg.REQUEST_PARAM_ILLEGAL + "已删除或服务不存在");
@@ -192,18 +189,12 @@ public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements
             throw new ForbiddenOperationException(ErrorInfo.Msg.FORBIDDEN_OPERATION + "服务已上架");
         }
         // 校验服务项和对应的服务类型是否已经启用
-        LambdaQueryWrapper<ServeItem> serveItemLambdaQueryWrapper = Wrappers.<ServeItem>lambdaQuery()
-                .eq(ServeItem::getId, serve.getServeItemId())
-                .eq(ServeItem::getActiveStatus, ServeEditStatus.ENABLE.getStatus())
-                .eq(ServeItem::getDelFlag, DelFlagEnum.NOT_DELETE.getStatus());
+        LambdaQueryWrapper<ServeItem> serveItemLambdaQueryWrapper = Wrappers.<ServeItem>lambdaQuery().eq(ServeItem::getId, serve.getServeItemId()).eq(ServeItem::getActiveStatus, ServeEditStatus.ENABLE.getStatus()).eq(ServeItem::getDelFlag, DelFlagEnum.NOT_DELETE.getStatus());
         ServeItem serveItem = serveItemMapper.selectOne(serveItemLambdaQueryWrapper);
         if (serveItem == null) {
             throw new ForbiddenOperationException(ErrorInfo.Msg.FORBIDDEN_OPERATION + "服务项禁用");
         }
-        LambdaQueryWrapper<ServeType> serveTypeLambdaQueryWrapper = Wrappers.<ServeType>lambdaQuery()
-                .eq(ServeType::getId, serveItem.getServeTypeId())
-                .eq(ServeType::getActiveStatus, ServeEditStatus.ENABLE.getStatus())
-                .eq(ServeType::getDelFlag, DelFlagEnum.NOT_DELETE.getStatus());
+        LambdaQueryWrapper<ServeType> serveTypeLambdaQueryWrapper = Wrappers.<ServeType>lambdaQuery().eq(ServeType::getId, serveItem.getServeTypeId()).eq(ServeType::getActiveStatus, ServeEditStatus.ENABLE.getStatus()).eq(ServeType::getDelFlag, DelFlagEnum.NOT_DELETE.getStatus());
         ServeType serveType = serveTypeMapper.selectOne(serveTypeLambdaQueryWrapper);
         if (serveType == null) {
             throw new ForbiddenOperationException(ErrorInfo.Msg.REQUEST_PARAM_ILLEGAL + "服务类型禁用");
