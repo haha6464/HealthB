5056befc473290fb870dc88349dff8bb943658e6
Dog2077 <1002302241@qq.com>
2024-07-17 13:32:04 +0800

    refactor:重构项目后端目录架构

---
+++ peizhen-backend/manage-service/src/test/java/com/example/baseaccompanying/ManageServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.baseaccompanying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
