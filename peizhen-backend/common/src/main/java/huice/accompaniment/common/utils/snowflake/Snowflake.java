5590990270eb06072b5720651bc53ee26aad4041
Dog2077 <1002302241@qq.com>
2024-07-19 14:14:48 +0800

    refactor:更改 pom group

--- peizhen-backend/common/src/main/java/huice/accompaniment/common/utils/snowflake/Snowflake.java
+++ peizhen-backend/common/src/main/java/huice/accompaniment/common/utils/snowflake/Snowflake.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
  * @Author welsir
  * @Date 2024/6/14 11:05
  */
-@Component("huice-snowflake")
+@Component
 public class Snowflake implements Serializable, IdGenerator<Long> {
 
     private static final long serialVersionUID = 1L;
